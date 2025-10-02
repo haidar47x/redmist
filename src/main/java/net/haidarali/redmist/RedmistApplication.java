f5be5edc6931063e0a1a55049e3e8e965a6ea2d2
Haidar Ali <haidar47x@gmail.com>
2025-10-02 23:48:00 +0500

    init: initial repo structure

---
+++ src/main/java/net/haidarali/redmist/RedmistApplication.java
@@ -0,0 +1,13 @@
+package net.haidarali.redmist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedmistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedmistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/haidarali/redmist/RedmistApplicationTests.java
@@ -0,0 +1,13 @@
+package net.haidarali.redmist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
